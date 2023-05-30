a3b665e0141cc4bf631ffea3da6387b7ed3290a6
mervecildag <merve.cildag94@gmail.com>
2023-05-30 16:51:14 +0200

    home-page design is changed.

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function openTab(evt, infoTabs) {
     document.getElementById(infoTabs).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
   
   document.getElementById("defaultOpen").click();
 
